7e5f5f3f4e0d3da1e9b975c6475906c96cf67678
sigxc <andrer.volod@gmail.com>
2025-02-12 13:18:47 +0300

    Initial commit

---
+++ Pendochine/machine.h
@@ -0,0 +1,81 @@
+#ifndef MACHINE_H
+#define MACHINE_H
+
+#include <stdint.h>
+
+#define mem_SIZE 65536
+#define NUM_REGISTERS 4
+
+typedef struct {
+} Registers;
+
+typedef struct {
+  uint8_t  mem[mem_SIZE];
+  uint32_t eip;
+  uint32_t eax;
+  uint32_t ebx;
+  uint32_t ecx;
+  uint32_t edx;
+  uint32_t flags : 8;
+} Machine;
+
+#define BIT(x) (1 << x)
+#define SET_FLAG(flags, flag) (flags |= flag)
+#define RESET_FLAG(flags, flag) (flags &= ~flag)
+#define SWITCH_FLAG(flags, flag) (flags ^= flag)
+#define CHECK_FLAG(flags, flag) (flags & flag)
+
+#define ZERO_FLAG BIT(0)
+#define SIGN_FLAG BIT(1)
+#define CARRY_FLAG BIT(2)
+#define OVERFLOW_FLAG BIT(3)
+#define PARITY_FLAG BIT(4)
+#define EQUAL_FLAG BIT(5)
+#define BIGGER_FLAG BIT(6)
+#define LESSER_FLAG BIT(7)
+
+typedef enum {
+  NOP,
+  LOAD,
+  LOADMEM,
+  STORE,
+  STOREMEM,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  POW,
+  SQRT,
+  JMP,
+  JZ,
+  JNZ,
+  JS,
+  JNS,
+  JC,
+  JNC,
+  JO,
+  JNO,
+  JP,
+  JNP,
+  INC,
+  DEC,
+  CALL,
+  CLZ,
+  CLC,
+  CLS,
+  CLO,
+  CLP,
+  CLE,
+  CLB,
+  CLL,
+  HALT = 0xFF,
+} Opcodes;
+
+typedef enum { EAX, EBX, ECX, EDX } Register;
+
+void init(Machine *machine);
+void regs_dump(Machine *machine);
+void mem_dump(Machine *machine);
+void execute(Machine *machine);
+
+#endif
---
+++ Pendochine/main.c
@@ -0,0 +1,17 @@
+#include "machine.h"
+
+int main(int argc, char *argv[]) {
+  Machine machine;
+  init(&machine);
+  machine.eax = 1;
+  machine.ebx = 2;
+  machine.mem[0] = ADD;
+  machine.mem[1] = EAX;
+  machine.mem[2] = EBX;
+  machine.mem[3] = HALT;
+
+  execute(&machine);
+  mem_dump(&machine);
+
+  return 0;
+}
